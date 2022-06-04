c5277b26ba17ea9a2cc8bc8de2725e6fe6d4ae47
Gaudencia Wanyama <conny@students.uonbi.ac.ke>
2022-06-05 01:56:57 +0300

    commit tree a6c266ac910d5e60d37d5cc1114e97fea6c72d79

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a ,b){
+ return a + b
+}
+console.log(add(1000, 500))
+function subtract(a, b){
+    return a - b
+}
+console.log(subtract(1000, 500))
+function multiply(a,b){
+    return a * b
+}
+console.log(multiply(20,50))
+
+function divide(a, b){
+    return a / b
+}
+console.log(multiply(200,50))
+function increment(n){
+    return n+=1
+}
+console.log(decrement(97))
+function decrement(n){
+    return n-=1
+}
+console.log(increment(97))
+function makeInt(n){
+    return n
+}
+console.log(makeInt(parseInt("20",10)))
+
+function makeInt(n){
+    return n
+}
+console.log(makeInt(parseInt("0x2328",10)))
